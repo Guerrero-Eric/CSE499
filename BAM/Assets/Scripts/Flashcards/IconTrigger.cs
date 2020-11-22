f8f691b67859ad4ca99821059038779141a28341
eg661 <eg6616@gmail.com>
2020-11-22 01:04:21 -0700

    library database connection

---
+++ BAM/Assets/Scripts/Flashcards/IconTrigger.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using UnityEngine.SceneManagement;
+
+
+public class IconTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private Touch touch;
+    private Image image;
+
+
+    // Update is called once per frame
+    void change()
+    {
+        
+        if(Input.touchCount == 0)
+        {
+            if (image)
+            {
+                image.enabled = false;
+            }
+        }
+
+        if (Input.touchCount == 1)
+        {
+            if (image)
+            {
+                image.enabled = true;
+            }
+        }
+    }
+}
